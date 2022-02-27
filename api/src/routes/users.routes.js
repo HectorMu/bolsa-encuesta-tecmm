2cc6727b51f88fb2581294841af436fa54627845
HectorMu <skyspartanblack@gmail.com>
2022-02-27 10:20:56 -0600

    Removed a dependency what caused conflict

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -2,20 +2,11 @@ const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/verifyToken");
 
-const { body } = require("express-validator");
-
 const controller = require("../controllers/users.controller");
 
 router.get("/users/getall", controller.GetAll);
 router.get("/users/getone/:id", controller.GetOne);
-router.post(
-  "/users/save",
-  body("correo").isEmail().withMessage("Correo no valido"),
-  body("clave")
-    .isLength({ min: 8 })
-    .withMessage("La contraseña debe tener minimo 8 caracteres de largo"),
-  controller.Save
-);
+router.post("/users/save", controller.Save);
 router.delete("/users/delete/:id", controller.Delete);
 router.put("/users/update/:id", controller.Update);
 
