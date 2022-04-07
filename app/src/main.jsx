805ca4059861b9a5ee6422d0b352556ecf31ea25
HectorMu <skyspartanblack@gmail.com>
2022-04-06 19:36:20 -0500

    Added scroll to top components to automatically scroll to top the page on change

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "@/components/Navigation/ScrollToTop";
 
 ReactDOM.render(
   <Router>
+    <ScrollToTop />
     <App />
   </Router>,
   document.getElementById("root")
