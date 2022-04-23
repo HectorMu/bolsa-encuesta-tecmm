613a2365f4542e7de8a95268d6217abeb3abd7cc
HectorMu <skyspartanblack@gmail.com>
2022-04-23 10:37:21 -0500

    Changed the layout of the graduated jobbank page, and added scroll to both sides

--- app/src/pages/Graduated/JobBank/JobBank.jsx
+++ app/src/pages/Graduated/JobBank/JobBank.jsx
@@ -43,7 +43,7 @@ const JobBank = () => {
         data-aos="flip-right"
         className="col-md-12 col-lg-12 col-xl-11 mx-auto"
       >
-        <div className="input-group w-100 mb-3">
+        {/* <div className="input-group w-100 mb-3">
           <input
             type="text"
             className="form-control bg-light"
@@ -58,14 +58,36 @@ const JobBank = () => {
               <i className="fas fa-search fa-sm"></i>
             </button>
           </div>
-        </div>
+        </div> */}
 
         <div className="card shadow rounded">
           <div className="card-body">
             <div className="row">
               <div className="col-md-5 col-lg-5 col-xl-5">
-                <div style={{ height: "65vh" }}>
-                  <div style={{ overflowY: "scroll", height: "100%" }}>
+                <div style={{ height: "63vh" }}>
+                  <div className="input-group w-100 mb-3 px-2 overflow-auto">
+                    <input
+                      type="text"
+                      className="form-control bg-light"
+                      placeholder="Buscar trabajos..."
+                      aria-label="Search"
+                      autoComplete="off"
+                      aria-describedby="basic-addon2"
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <div className="input-group-append">
+                      <button className="btn btn-primary" type="button">
+                        <i className="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div
+                    className="purple-scroll"
+                    style={{
+                      overflowY: "scroll",
+                      maxHeight: "55vh",
+                    }}
+                  >
                     <List
                       isLoading={isLoading}
                       searchTerm={searchTerm}
@@ -75,7 +97,15 @@ const JobBank = () => {
                 </div>
               </div>
               <div className="col-md-7 col-lg-7 col-xl-7">
-                <Showcase />
+                <div
+                  className="purple-scroll"
+                  style={{
+                    overflowY: "scroll",
+                    maxHeight: "55vh",
+                  }}
+                >
+                  <Showcase />
+                </div>
               </div>
             </div>
           </div>
