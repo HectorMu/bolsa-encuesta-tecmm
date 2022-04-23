548ba6eb596c60b907ba0a02431ebe6a38ba156a
HectorMu <skyspartanblack@gmail.com>
2022-04-23 13:17:10 -0500

    Added the new design of the graduated jobbank dashboard

--- app/src/pages/Graduated/JobBank/Postulations.jsx
+++ app/src/pages/Graduated/JobBank/Postulations.jsx
@@ -21,29 +21,35 @@ const Postulations = () => {
         data-aos="flip-right"
         className="col-md-12 col-lg-12 col-xl-11 mx-auto"
       >
-        <div className="input-group w-100 mb-3">
-          <input
-            type="text"
-            className="form-control bg-light"
-            placeholder="Buscar en mis postulaciones..."
-            aria-label="Search"
-            autoComplete="off"
-            aria-describedby="basic-addon2"
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <div className="input-group-append">
-            <button className="btn btn-primary" type="button">
-              <i className="fas fa-search fa-sm"></i>
-            </button>
-          </div>
-        </div>
-
         <div className="card shadow rounded">
           <div className="card-body">
             <div className="row">
               <div className="col-md-5 col-lg-5 col-xl-5">
-                <div style={{ height: "65vh" }}>
-                  <div style={{ overflowY: "scroll", height: "100%" }}>
+                <div style={{ height: "63vh" }}>
+                  <div className="input-group w-100 mb-3">
+                    <input
+                      type="text"
+                      className="form-control bg-light"
+                      placeholder="Buscar en mis postulaciones..."
+                      aria-label="Search"
+                      autoComplete="off"
+                      aria-describedby="basic-addon2"
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <div className="input-group-append">
+                      <button className="btn btn-primary" type="button">
+                        <i className="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <div
+                    className="purple-scroll"
+                    style={{
+                      overflowY: "scroll",
+                      maxHeight: "55vh",
+                    }}
+                  >
                     <List
                       postulations={postulations}
                       isLoading={isLoading}
@@ -53,7 +59,15 @@ const Postulations = () => {
                 </div>
               </div>
               <div className="col-md-7 col-lg-7 col-xl-7">
-                <Showcase refreshData={refreshData} />
+                <div
+                  className="purple-scroll"
+                  style={{
+                    overflowY: "scroll",
+                    maxHeight: "55vh",
+                  }}
+                >
+                  <Showcase refreshData={refreshData} />
+                </div>
               </div>
             </div>
           </div>
