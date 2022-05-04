bc3f208083209ec77742f40c9ee92d34280ddff2
HectorMu <skyspartanblack@gmail.com>
2022-05-04 16:19:29 -0500

    Fixed a writing error

--- api/src/routes/company.jobs.routes.js
+++ api/src/routes/company.jobs.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/verifyToken");
-const isCompany = require("../middlewares/isCompany");
+const isCompany = require("../middlewares/IsCompany");
 const isCompanyOrAdmin = require("../middlewares/isCompanyOrAdmin");
 
 const controller = require("../controllers/company.jobs.controller");
