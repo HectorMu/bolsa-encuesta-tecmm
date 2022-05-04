97d526c67a8bba2981db24dbf0a34bfb92a3cd2a
HectorMu <skyspartanblack@gmail.com>
2022-05-04 17:34:14 -0500

    keep fixing the db connection

--- api/src/config/keys.js
+++ api/src/config/keys.js
@@ -3,11 +3,12 @@ const keys = {
     host: "localhost",
     user: "root",
     password: "",
-    port: "'/var/run/mysqld/mysqld.sock'",
+    port: 3306,
     database: "control_egresados",
   },
   production: {
     host: process.env.DB_HOST,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
