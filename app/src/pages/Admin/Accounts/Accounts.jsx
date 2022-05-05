6ab75f63efce2570916ac02dfce1febf8d0f38c1
HectorMu <skyspartanblack@gmail.com>
2022-05-05 10:07:59 -0500

    fixed a button on accounts admin view

--- app/src/pages/Admin/Accounts/Accounts.jsx
+++ app/src/pages/Admin/Accounts/Accounts.jsx
@@ -4,11 +4,11 @@ import List from "../../../containers/Admin/Accounts/List";
 const Users = () => {
   return (
     <div className="container-fluid">
-      <div className="d-sm-flex align-items-center justify-content-between mb-4">
+      <div className="d-flex flex-column flex-lg-row flex-md-row flex-xl-row align-items-center justify-content-between mb-4">
         <h1 className="h3 mb-0 text-gray-800">Cuentas</h1>
         <Link
           to={"/accounts/add"}
-          className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+          className="btn btn-sm btn-primary shadow-sm mt-3 mt-md-0 mt-lg-0 mt-xl-0"
         >
           <i className="fas fa-plus fa-sm text-white"></i> Nueva cuenta
         </Link>
