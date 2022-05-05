d207e1ddf11e4e7b2f045b902d2ab4da82e3c3b6
HectorMu <skyspartanblack@gmail.com>
2022-05-04 21:00:03 -0500

    Removed api js from repo

--- app/src/services/API.js
+++
@@ -1,9 +0,0 @@
-//Dev
-export const Host = "http://localhost:4000";
-export const API = `${Host}/api`;
-
-//Test on network
-// const baseUrl = `${Host}/api`;
-
-//production
-// export const baseUrl = "http://domain.com/api",
