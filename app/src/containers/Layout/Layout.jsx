2955c78acaa950f9d78a0962159af3a7061335e0
HectorMu <skyspartanblack@gmail.com>
2022-05-16 01:24:20 -0500

    Added more accesibility options to app to movile views and for desktop view

--- app/src/containers/Layout/Layout.jsx
+++ app/src/containers/Layout/Layout.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import Navbar from "../Navigation/Navbar";
 import CustomSidebar from "../Navigation/CustomSidebar";
+import useWindowSize from "@/hooks/useWindowResize";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   const [sidebarToggled, setSidebarToggled] = useState(false);
+  const size = useWindowSize();
 
-  const handleSidebarToggle = () => {
-    setSidebarToggled(!sidebarToggled);
+  const handleSidebarToggle = () => setSidebarToggled(!sidebarToggled);
+
+  const handleToggleOnBody = (e) => {
+    switch (e.detail) {
+      case 1:
+        if (size.width < 600 && sidebarToggled) setSidebarToggled(false);
+        break;
+    }
   };
 
   return (
-    <div id="wrapper" className="wrapper">
+    <div id="wrapper" onClick={handleToggleOnBody} className="wrapper">
       <CustomSidebar sidebarControl={{ sidebarToggled, handleSidebarToggle }} />
       <div id="content-wrapper" className="content d-flex flex-column">
         <Navbar sidebarControl={{ sidebarToggled, handleSidebarToggle }} />
--- app/src/containers/Navigation/CustomSidebar.jsx
+++ app/src/containers/Navigation/CustomSidebar.jsx
@@ -1,17 +1,45 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import useSession from "@/hooks/useSession";
+import useWindowSize from "@/hooks/useWindowResize";
 
 const CustomSidebar = ({
   sidebarControl: { sidebarToggled, handleSidebarToggle },
 }) => {
   const { user } = useSession();
+  const size = useWindowSize();
+
+  const handleSidebarToggleOnLink = () =>
+    size.width < 900 && handleSidebarToggle(!sidebarToggled);
 
   if (user === null) {
     return <></>;
   }
   return (
     <aside className={`c-sidebar  ${sidebarToggled ? `active ` : ``} `}>
+      <button
+        onClick={() => handleSidebarToggle(!sidebarToggled)}
+        className="btn btn-primary rounded-right floating-btn btn-sm p-0 py-4"
+      >
+        {size.width < 600 ? (
+          <>
+            {" "}
+            {sidebarToggled ? (
+              <i className="fas fa-chevron-left"></i>
+            ) : (
+              <i className="fas fa-chevron-right"></i>
+            )}
+          </>
+        ) : (
+          <>
+            {sidebarToggled ? (
+              <i className="fas fa-chevron-right"></i>
+            ) : (
+              <i className="fas fa-chevron-left"></i>
+            )}
+          </>
+        )}
+      </button>
       <div className="d-flex justify-content-end"></div>
       {/* Opciones para administrador */}
       {user.fk_rol === 1 ? (
@@ -20,9 +48,10 @@ const CustomSidebar = ({
             <NavLink
               to="/"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-fw fa-tachometer-alt"></i> Dashboard
+              <i className="fas fa-fw fa-tachometer-alt"></i>{" "}
+              <span>Dashboard</span>
             </NavLink>
             <hr className="c-sidebar-divider" />
             <div className="heading">
@@ -31,23 +60,23 @@ const CustomSidebar = ({
             <NavLink
               to="/accounts"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-users"></i> Cuentas
+              <i className="fas fa-users"></i> <span>Cuentas</span>
             </NavLink>
             <NavLink
               to="/companies"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-building"></i> Empresas
+              <i className="fas fa-building"></i> <span>Empresas</span>
             </NavLink>
             <NavLink
               to="/graduates"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-user-graduate"></i> Egresados
+              <i className="fas fa-user-graduate"></i> <span>Egresados</span>
             </NavLink>
             <hr className="c-sidebar-divider" />
             <div className="heading">
@@ -56,9 +85,9 @@ const CustomSidebar = ({
             <NavLink
               to="/jobbank"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-envelope"></i> Postulaciones
+              <i className="fas fa-envelope"></i> <span>Postulaciones</span>
             </NavLink>{" "}
             <hr className="c-sidebar-divider" />
             <div className="heading">
@@ -67,9 +96,9 @@ const CustomSidebar = ({
             <NavLink
               to="/surveys/reports/"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-print"></i> Reportes
+              <i className="fas fa-print"></i> <span>Reportes</span>
             </NavLink>
             <hr className="c-sidebar-divider" />
           </nav>
@@ -83,9 +112,9 @@ const CustomSidebar = ({
             <NavLink
               to="/graduated/survey"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-poll-h"></i> Encuesta
+              <i className="fas fa-poll-h"></i> <span>Encuesta</span>
             </NavLink>
             <hr className="c-sidebar-divider" />
             <div className="heading">
@@ -94,16 +123,16 @@ const CustomSidebar = ({
             <NavLink
               to="/graduated/jobbank/jobs"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-briefcase"></i> Trabajos
+              <i className="fas fa-briefcase"></i> <span>Trabajos</span>
             </NavLink>
             <NavLink
               to="/graduated/jobbank/postulations"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-envelope"></i> Mis postulaciones
+              <i className="fas fa-envelope"></i> <span>Mis postulaciones</span>
             </NavLink>
             <hr className="c-sidebar-divider" />
           </nav>
@@ -117,17 +146,18 @@ const CustomSidebar = ({
             <NavLink
               to="/"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-fw fa-tachometer-alt"></i> Dashboard
+              <i className="fas fa-fw fa-tachometer-alt"></i>{" "}
+              <span>Dashboard</span>
             </NavLink>
             <hr className="c-sidebar-divider" />
             <NavLink
               to="/company/survey"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-poll-h"></i> Encuesta
+              <i className="fas fa-poll-h"></i> <span>Encuesta</span>
             </NavLink>
 
             <hr className="c-sidebar-divider" />
@@ -138,9 +168,9 @@ const CustomSidebar = ({
             <NavLink
               to="/company/jobbank"
               className="menu-item"
-              onClick={() => handleSidebarToggle(!sidebarToggled)}
+              onClick={handleSidebarToggleOnLink}
             >
-              <i className="fas fa-briefcase"></i> Mis vacantes
+              <i className="fas fa-briefcase"></i> <span>Mis vacantes</span>
             </NavLink>
             <hr className="c-sidebar-divider" />
           </nav>
